2b1a6ca2d8e1bd08057a752866cda7ac91147b31
Danielss89 <Danielss89@gmail.com>
2014-07-19 15:39:05 +0200

    Fix config path

--- src/E4W/Sms/Module.php
+++ src/E4W/Sms/Module.php
@@ -8,12 +8,12 @@ class Module
 {
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
+        return include __DIR__ . '/../../../config/module.config.php';
     }
 
     public function getServiceConfig()
     {
-        return include __DIR__ . '/config/module.service.php';
+        return include __DIR__ . '/../../../config/module.service.php';
     }
 
     public function getAutoloaderConfig()
